21a200b26beed62d93b644ad7a9f0652191a6699
User8300 <User8300@DESKTOP-UCTHAGD>
2019-02-18 12:18:39 +0100

    Initiation on project

---
+++ BeMyValentine/src/bemyvalentine/BeMyValentine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bemyvalentine;
+
+/**
+ *
+ * @author User8300
+ */
+public class BeMyValentine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
