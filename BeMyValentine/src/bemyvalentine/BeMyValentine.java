bcb0cb9b3465e925a8f9231c476b73dedff0cf75
User8300 <User8300@DESKTOP-UCTHAGD>
2019-02-18 12:54:45 +0100

    set the sliders

--- BeMyValentine/src/bemyvalentine/BeMyValentine.java
+++ BeMyValentine/src/bemyvalentine/BeMyValentine.java
@@ -15,7 +15,10 @@ public class BeMyValentine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+Parameters frame=new Parameters();
+frame.pack();
+frame.setVisible(true);
+// TODO code application logic here
     }
     
 }
